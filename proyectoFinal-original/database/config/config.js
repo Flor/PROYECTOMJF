30453d23a8a25827aeee5b33fa325beba61df9a1
Ignacio Romani <promani7@gmail.com>
2021-06-21 19:22:35 -0300

    mf

--- proyectoFinal-original/database/config/config.js
+++ proyectoFinal-original/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "redcatanox",
+    "password": "root",
     "database": "proyectoFinal",
     "host": "127.0.0.1",
     "dialect": "mysql"
